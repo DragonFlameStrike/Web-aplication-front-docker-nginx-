c9606bd69ed1e952e98957c95cd090ffc06dc29f
DragonFlameStrike <d.pankov@g.nsu.ru>
2022-05-07 01:03:11 +0700

    Add main controller which renders html pages with css. Add Banner Entity, connected MySQL DataBase.

---
+++ testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/controllers/MainController.java
@@ -0,0 +1,28 @@
+package com.jarsoft.webapp.adverts.testtask.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class MainController {
+
+//    @GetMapping("/")
+//    public String startPage(){
+//        return "layout/emptyView";
+//    }
+
+    @GetMapping("/")
+    public String mainView(HttpServletRequest request) {
+        System.out.println(request.getRemoteAddr()); // IP ADDRESS
+        System.out.println(request.getHeader("User-Agent")); // USER AGENT
+        return "layout/emptyView";
+    }
+    @GetMapping("/categories")
+    public String categoriesView(){
+        return "layout/categories";
+    }
+}
---
+++ testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/entity/BannerEntity.java
@@ -0,0 +1,44 @@
+package com.jarsoft.webapp.adverts.testtask.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BannerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long IdBanner;
+    private String name;
+    private Long price;
+
+    public BannerEntity() {
+    }
+
+    public Long getIdBanner() {
+        return IdBanner;
+    }
+
+    public void setIdBanner(Long idBanner) {
+        IdBanner = idBanner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+}
