96f03d7f9234848b76e21b67765ee173764d369c
DragonFlameStrike <d.pankov@g.nsu.ru>
2022-05-14 23:37:10 +0700

    Add bad realisation NotUniqueNameException in CategoriesController.java (create and edit)

--- testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/controllers/CategoriesController.java
+++ testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/controllers/CategoriesController.java
@@ -2,6 +2,7 @@ package com.jarsoft.webapp.adverts.testtask.controllers;
 
 import com.jarsoft.webapp.adverts.testtask.entity.BannerEntity;
 import com.jarsoft.webapp.adverts.testtask.entity.CategoryEntity;
+import com.jarsoft.webapp.adverts.testtask.exception.NotUniqueNameException;
 import com.jarsoft.webapp.adverts.testtask.exception.ResourceNotFoundException;
 import com.jarsoft.webapp.adverts.testtask.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController()
@@ -54,7 +56,18 @@ public class CategoriesController {
     }
 
     @PostMapping("/{cid}")
-    public ResponseEntity<CategoryEntity> updateCategory(@PathVariable Long cid, @Valid @RequestBody CategoryEntity categoryDetails){
+    public ResponseEntity<CategoryEntity> updateCategory(@PathVariable Long cid, @Valid @RequestBody CategoryEntity categoryDetails) throws NotUniqueNameException {
+        Iterable<CategoryEntity> categories = categoryRepository.findAll();
+        for (CategoryEntity category: categories) {
+            if(!Objects.equals(category.getIdCategory(), cid) && (category.getDeleted() == null || !category.getDeleted())) {
+                if (category.getName().equals(categoryDetails.getName())) {
+                    throw new NotUniqueNameException();
+                }
+                if (category.getIdRequest().equals(categoryDetails.getIdRequest())) {
+                    throw new NotUniqueNameException();
+                }
+            }
+        }
         CategoryEntity category = categoryRepository.findById(cid).orElseThrow();
         category.setName(categoryDetails.getName());
         category.setIdRequest(categoryDetails.getIdRequest());
@@ -68,13 +81,27 @@ public class CategoriesController {
     public String create() {return "";}
 
     @PostMapping("/create")
-    public CategoryEntity createCategory(@Valid @RequestBody CategoryEntity category) {
-        return categoryRepository.save(category);
+    public CategoryEntity createCategory(@Valid @RequestBody CategoryEntity newCategory) throws NotUniqueNameException {
+        Iterable<CategoryEntity> categories = categoryRepository.findAll();
+        for (CategoryEntity category: categories) {
+            if(category.getDeleted() == null || !category.getDeleted()) {
+                if (category.getName().equals(newCategory.getName())) {
+                    throw new NotUniqueNameException();
+                }
+                if (category.getIdRequest().equals(newCategory.getIdRequest())) {
+                    throw new NotUniqueNameException();
+                }
+            }
+        }
+        return categoryRepository.save(newCategory);
     }
     @DeleteMapping("/{cid}")
-    public ResponseEntity<Boolean> deleteCategory(@PathVariable Long cid){
+    public ResponseEntity<Boolean> deleteCategory(@PathVariable Long cid) throws NotUniqueNameException {
         CategoryEntity category = categoryRepository.findById(cid)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + cid));
+//        if(){
+//            throw new NotUniqueNameException();
+//        }
         category.setDeleted(true);
         categoryRepository.save(category);
         return ResponseEntity.ok(true);
