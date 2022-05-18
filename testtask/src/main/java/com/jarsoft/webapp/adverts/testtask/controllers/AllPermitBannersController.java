c684532e4934905aba50a257328091ccd3619142
DragonFlameStrike <d.pankov@g.nsu.ru>
2022-05-18 21:45:37 +0700

    If there is no banner to response the server returns HTTP status 204

--- testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/controllers/AllPermitBannersController.java
+++ testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/controllers/AllPermitBannersController.java
@@ -1,19 +1,14 @@
 package com.jarsoft.webapp.adverts.testtask.controllers;
 
 import com.jarsoft.webapp.adverts.testtask.entity.BannerEntity;
-import com.jarsoft.webapp.adverts.testtask.exception.BadNameException;
 import com.jarsoft.webapp.adverts.testtask.repositories.BannerRepository;
-import com.jarsoft.webapp.adverts.testtask.security.SqlInjectionSecurity;
-import org.apache.commons.lang3.ArrayUtils;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 import java.util.stream.StreamSupport;
 
 @RestController()
@@ -23,16 +18,21 @@ public class AllPermitBannersController {
     private BannerRepository bannerRepository;
 
     @GetMapping("")
-    public BannerEntity viewBannerWithCategory(HttpServletRequest request,@RequestParam List<String> cat) {
+    public Optional<BannerEntity> viewBannerWithCategory(HttpServletRequest request, HttpServletResponse response, @RequestParam List<String> cat) {
         System.out.println(request.getRemoteAddr()); // IP ADDRESS
         System.out.println(request.getHeader("User-Agent"));
         System.out.println(cat);
+        Optional<BannerEntity> finilBanner = null;
         Iterable<BannerEntity> banners = bannerRepository.findAll();
-        List<BannerEntity> output = StreamSupport.stream(banners.spliterator(), false)
+        List<BannerEntity> BannersWithCategories = StreamSupport.stream(banners.spliterator(), false)
                 .filter(banner -> banner.getCategories().stream()
                         .anyMatch(category -> cat.stream().anyMatch(str -> str.trim().equals(category.getIdRequest()))) && !banner.getDeleted()).toList();
 
-        return output.stream()
-                .max(Comparator.comparing(BannerEntity::getPrice)).orElseThrow(NoSuchElementException::new);
+
+        if(BannersWithCategories.isEmpty()) response.setStatus( HttpStatus.SC_NO_CONTENT);
+        else{
+            finilBanner = BannersWithCategories.stream().max(Comparator.comparing(BannerEntity::getPrice));
+        }
+        return finilBanner;
     }
 }
