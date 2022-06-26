60b3b9900bc90e0583604981124b3b3221bf0556
DragonFlameStrike <d.pankov@g.nsu.ru>
2022-06-26 21:13:04 +0700

    Add environment to get login and password from application.properties

--- testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/security/SecurityConfiguration.java
+++ testtask/src/main/java/com/jarsoft/webapp/adverts/testtask/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.jarsoft.webapp.adverts.testtask.security;
 
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,11 +14,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+    @Autowired
+    private Environment env;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
-                .withUser("root")
-                .password("root")
+                .withUser(env.getProperty("spring.security.user.name"))
+                .password(env.getProperty("spring.security.user.password"))
                 .roles("ROOT");
     }
 
